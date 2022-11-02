35161e694428b47ac6f6fe149afc6ebb6998ae68
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-11-02 15:26:30 +0530

    Select option to create, delete, update, print the addressbook

--- Program.cs
+++ Program.cs
@@ -4,7 +4,53 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program in AddressBookMain class");
+            bool Enter = true;
+            AdressBook1 ab = StartProgram();
+
+            Console.WriteLine("--------- AdressBook 1.0 ---------");
+
+            while (Enter)
+            {
+                PrintUserOptions();
+                var userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        ab.CreateUser();
+                        break;
+                    case "2":
+                        ab.UpdateUserInformation();
+                        break;
+                    case "3":
+                        ab.RemovePersonFromList();
+                        break;
+                    case "4":
+                        ab.ShowAllPersonsInList();
+                        break;
+                    default:
+                        Enter = false;
+                        break;
+                }
+            }
+        }
+
+        private static void PrintUserOptions()
+        {
+            Console.WriteLine("Choose one of the following options: ");
+            Console.WriteLine("#1 Create new user");
+            Console.WriteLine("#2 Edit user information");
+            Console.WriteLine("#3 Delete existing user");
+            Console.WriteLine("#4 Show all users in adressBook");
+        }
+
+        private static AdressBook1 StartProgram()
+        {
+            AdressBook1 ab = new AdressBook1();
+
+            return ab;
         }
     }
 }
+    
+
